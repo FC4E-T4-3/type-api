b92cfa34e20a4ecab09bd2d0075a2e10e84120ed
Hans Lienhop <hans.lienhop@gmail.com>
2023-10-19 12:09:48 +0200

    little Cleanup. UnitEntity class done.

--- src/main/java/com/fce4/dtrtoolkit/TypeEntity.java
+++ src/main/java/com/fce4/dtrtoolkit/TypeEntity.java
@@ -2,8 +2,6 @@ package com.fce4.dtrtoolkit;
 
 import java.util.ArrayList;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.HashMap;
@@ -22,8 +20,7 @@ public class TypeEntity {
     private ArrayList<String> aliases;
     private JsonNode content;
 
-    public TypeEntity(String pid, String prefix, String type, JsonNode content, String style, String origin, String name)
-    {
+    public TypeEntity(String pid, String prefix, String type, JsonNode content, String style, String origin, String name) {
         this.pid = pid;
         this.type = type;
         this.content = content;
@@ -32,8 +29,7 @@ public class TypeEntity {
         this.name = name;
     }
 
-    public TypeEntity(JsonNode node, String style, String origin)
-    {
+    public TypeEntity(JsonNode node, String style, String origin) {
         this.pid = node.get("id").textValue();
         this.type = node.get("type").textValue();
         this.content = node.get("content");
@@ -42,8 +38,7 @@ public class TypeEntity {
         this.origin = origin;
     }
 
-    public TypeEntity(JsonNode node, String origin)
-    {
+    public TypeEntity(JsonNode node, String origin) {
         this.pid = node.get("id").textValue();
         this.type = node.get("type").textValue();
         this.content = node.get("content");
@@ -52,8 +47,7 @@ public class TypeEntity {
         this.origin = origin;
     }
 
-    public TypeEntity(Map<String, Object> node)
-    {
+    public TypeEntity(Map<String, Object> node) {
         ObjectMapper mapper = new ObjectMapper();
         this.pid = node.get("id").toString();
         this.type = node.get("type").toString();
@@ -66,57 +60,88 @@ public class TypeEntity {
         this.origin = node.get("origin").toString();
     }
 
-    public String getPid(){
+
+    public String getPid() {
         return this.pid;
     }
 
-    public String getSchema(){
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getType() {
         return this.type;
     }
 
-    public JsonNode getContent(){
-        return this.content;
+    public void setType(String type) {
+        this.type = type;
     }
 
-    public String getStyle(){
+    public String getStyle() {
         return this.style;
     }
 
-    public String getName(){
-        return this.name;
-    }
-
-    public String getOrigin(){
-        return this.origin;
-    }
-
-    public String getDescription(){
-        return this.desc;
-    }
-
-	public long getDate(){
-		return this.date;
-	}
-
-    public void setStyle(String style){
+    public void setStyle(String style) {
         this.style = style;
     }
 
-	public void setDescription(String desc){
-		this.desc = desc;
-	}
+    public String getOrigin() {
+        return this.origin;
+    }
 
-    public void setAuthors(ArrayList<String> authors){
-		this.authors = authors;
-	}
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
 
-	public void setAliases(ArrayList<String> aliases){
-		this.aliases = aliases;
-	}
+    public String getName() {
+        return this.name;
+    }
 
-	public void setDate(long date){
-		this.date = date;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getDate() {
+        return this.date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+
+    public String getDesc() {
+        return this.desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public ArrayList<String> getAuthors() {
+        return this.authors;
+    }
+
+    public void setAuthors(ArrayList<String> authors) {
+        this.authors = authors;
+    }
+
+    public ArrayList<String> getAliases() {
+        return this.aliases;
+    }
+
+    public void setAliases(ArrayList<String> aliases) {
+        this.aliases = aliases;
+    }
+
+    public JsonNode getContent() {
+        return this.content;
+    }
+
+    public void setContent(JsonNode content) {
+        this.content = content;
+    }
+
+
     /**
      * Serializes a TypeObject to a JsonNode object. The setters and getters are required for the mapper.
      * @return a JsonNode representing the TypeObject.
@@ -128,8 +153,6 @@ public class TypeEntity {
     }
 
     public HashMap<String,Object> serializeSearch() throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-
         HashMap<String, Object> typeSearch = new HashMap<>();
         typeSearch.put("id", this.pid);
         typeSearch.put("name", this.name);
---
+++ src/main/java/com/fce4/dtrtoolkit/UnitEntity.java
@@ -0,0 +1,155 @@
+package com.fce4.dtrtoolkit;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class UnitEntity {
+    private String pid;
+    private String type;
+    private String origin;
+    private String name = "";
+    private long date = 0;
+    private String desc = "";
+    private ArrayList<String> authors;
+    private String unitSymbol;
+    private String quantity;
+    private String quantitySymbol;
+
+    public UnitEntity(String pid, String type, String origin, String name, long date, 
+                        String desc, ArrayList<String> authors, String unitSymbol, 
+                        String quantity, String quantitySymbol){
+        this.pid = pid;
+        this.type = type;
+        this.origin = origin;
+        this.name = name;
+        this.date = date;
+        this.desc = desc;
+        this.authors = authors;
+        this.unitSymbol = unitSymbol;
+        this.quantity = quantity;
+        this.quantitySymbol = quantitySymbol;
+    }
+
+    public UnitEntity(Map<String, Object> unit){
+        this.pid = unit.get("id").toString();
+        this.type = unit.get("type").toString();
+        this.origin = unit.get("origin").toString();
+        this.name = unit.get("name").toString();
+        this.date = Long.parseLong(unit.get("date").toString());
+        this.desc = unit.get("desc").toString();
+        this.authors = (ArrayList<String>) unit.get("authors");
+        this.unitSymbol = unit.get("unitSymbol").toString();
+        this.quantity = unit.get("quantity").toString();
+        this.quantitySymbol = unit.get("quantitySymbol").toString();
+    }
+
+
+    public String getPid() {
+        return this.pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getOrigin() {
+        return this.origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getDate() {
+        return this.date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+
+    public String getDesc() {
+        return this.desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public ArrayList<String> getAuthors() {
+        return this.authors;
+    }
+
+    public void setAuthors(ArrayList<String> authors) {
+        this.authors = authors;
+    }
+
+    public String getUnitSymbol() {
+        return this.unitSymbol;
+    }
+
+    public void setUnitSymbol(String unitSymbol) {
+        this.unitSymbol = unitSymbol;
+    }
+
+    public String getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getQuantitySymbol() {
+        return this.quantitySymbol;
+    }
+
+    public void setQuantitySymbol(String quantitySymbol) {
+        this.quantitySymbol = quantitySymbol;
+    }
+     
+    /**
+     * Serializes a UnitObject to a JsonNode object. The setters and getters are required for the mapper.
+     * @return a JsonNode representing the UNitObject.
+     */
+    public JsonNode serialize() {
+        ObjectMapper mapper = new ObjectMapper(); 
+        JsonNode node = mapper.convertValue(this, JsonNode.class);
+        return node;
+    }
+
+    public HashMap<String,Object> serializeSearch() throws Exception {
+        HashMap<String, Object> typeSearch = new HashMap<>();
+        typeSearch.put("id", this.pid);
+        typeSearch.put("name", this.name);
+        typeSearch.put("type", this.type);
+        typeSearch.put("date", this.date);
+        typeSearch.put("desc", this.desc);
+        typeSearch.put("origin", this.origin);
+        typeSearch.put("authors", this.authors.toArray(new String[0]));
+        typeSearch.put("unitSymbol", this.unitSymbol);
+        typeSearch.put("quantity", this.quantity);
+        typeSearch.put("quantitySymbol", this.quantitySymbol);
+        return typeSearch;
+    }
+}
