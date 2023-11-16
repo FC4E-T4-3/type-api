9465bf9fd2e590dbd72111a3dc6d92a0844d42af
Hans Lienhop <hans.lienhop@gmail.com>
2023-11-16 11:44:38 +0100

    added xml support to errormessage from exception handler

--- src/main/java/com/fce4/dtrtoolkit/ErrorHandling/ControllerExceptionHandler.java
+++ src/main/java/com/fce4/dtrtoolkit/ErrorHandling/ControllerExceptionHandler.java
@@ -6,21 +6,29 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.typesense.api.exceptions.ObjectNotFound;
+import org.springframework.http.MediaType;
 import java.util.Date;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-  @ExceptionHandler(value = {IOException.class})
-  public ResponseEntity<ErrorMessage> badRequestException(IOException ex, WebRequest request) {    
-    ErrorMessage message = new ErrorMessage(
+@ExceptionHandler(value = {IOException.class})
+    public ResponseEntity<Object> badRequestException(IOException ex, WebRequest request) {
+        final HttpHeaders responseHeaders = new HttpHeaders();
+        ErrorMessage message = new ErrorMessage(
         HttpStatus.BAD_REQUEST.value(),
         new Date(),
         ex.getMessage(),
         request.getDescription(false));
-    ResponseEntity<ErrorMessage> response = new ResponseEntity<ErrorMessage>(message, HttpStatus.BAD_REQUEST);
-    return response;
-  }
+        System.out.println(message.toXML());
+        if(request.getHeader("Accept") != null){
+            if(request.getHeader("Accept").equals("application/xml")){
+                responseHeaders.setContentType(MediaType.APPLICATION_XML);
+                return new ResponseEntity<Object>(message.toXML(), responseHeaders, HttpStatus.BAD_REQUEST);
+            }
+        }
+        return new ResponseEntity<Object>(message, HttpStatus.BAD_REQUEST); 
+    }
 }
